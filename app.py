1c3a153e0b5db1616792761399c6ab8891f7fb0c
Yasmine ALAMI MEJJATI <y.alami-mejjati@tbs-education.org>
2025-03-20 16:49:44 +0000

    Read the Datasets

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-st.title("CSV File Uploader")
+st.title("Import a CSV File and Visualize with Streamlit Charts ")
 
-uploaded_file = st.file_uploader("upload a csv file", type=["csv"])
+df = pd.read_csv("Datasets/trips_data_1000.csv")
+st.write(" Preview Uploaded data")
+st.dataframe(df.head())
 
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)  # Use the uploaded file variable directly
-    st.write("### Preview of Uploaded Data:")
-    st.dataframe(df.head())
+# Chart 1: Bar chart of customers by country
+st.subheader("Customers by City")
+country_counts = df['customer_city'].value_counts()
+st.write(country_counts)
+st.bar_chart(country_counts)
+
+# Chart 2: Line chart of Tripvcs over time
+st.subheader("Subscriptions Over Time")
+df['Trips Date'] = pd.to_datetime(df['pickup_time']).dt.date
+subscriptions_by_date = df.groupby('Trips Date').size().reset_index(name='Count')
+st.write(subscriptions_by_date)
+st.line_chart(subscriptions_by_date, x="Trips Date", y="Count")
